dd12ab8c659759c63dbcd0e86b98d2b4d511d253
harshaVardhanSatuluri <vardhansinbox@gmail.com>
2025-02-06 19:54:52 +0530

    updated controller and routes files

--- routes/thoughtsRoutes.js
+++ routes/thoughtsRoutes.js
@@ -1,9 +1,12 @@
+// routes/thoughtsRoutes.js
 import express from "express";
-import { getThoughts, addThought } from "../controllers/thoughtsController.js";
+import { getThoughts, addThought, updateThought, deleteThought } from "../controllers/thoughtsController.js";
 
 const router = express.Router();
 
 router.get("/", getThoughts);
 router.post("/", addThought);
+router.put("/:id", updateThought);
+router.delete("/:id", deleteThought);
 
 export default router;
